36ceaeeda1e186627c27f7d79e0b296c2db30981
mmucklo <mmucklo@gmail.com>
2019-08-16 17:24:07 -0700

    add Dockerfile(s), caddy setup, etc. (#1)

---
+++ gp1/php/caddy.go
@@ -0,0 +1,8 @@
+package main
+
+import "github.com/caddyserver/caddy/caddy/caddymain"
+
+func main() {
+    caddymain.EnableTelemetry = false
+    caddymain.Run()
+}
---
+++ gp1/php/entrypoint.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+/usr/local/sbin/php-fpm
+/usr/local/bin/caddy -agree=true
